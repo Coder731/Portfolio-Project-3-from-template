f15e15905b3e004351794d3eb34b47665e26e159
Coder731 <stephenobrien1@gmail.com>
2021-09-08 22:02:10 +0000

    Add run py basic code for board

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+# Step 0 Build Board
+board = [[1,2,3],
+         [4,5,6],
+         [7,8,9]]
+
+# Step 1 Check User Selection:
+print("User %d enter your move (1-9):")
