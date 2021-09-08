78e35c674e873fc81416f89e5a8ee9f6587aa276
Coder731 <stephenobrien1@gmail.com>
2021-09-08 23:00:35 +0000

    Add line break

--- run.py
+++ run.py
@@ -4,9 +4,10 @@
 
 
 # Step 0 Build Board
-board = [[1, 2, 3],
-         [4, 5, 6],
-         [7, 8, 9]]
+board = [[1, 2, 3],\n
+         [4, 5, 6],\n
+         [7, 8, 9],\n]
 
 # Step 1 Check User Selection:
 print("User %d enter your move (1-9):")
+print(board)
