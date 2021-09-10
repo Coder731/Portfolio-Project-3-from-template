8b228fc59cb454c17ae5ddc8937d390b07732591
Coder731 <stephenobrien1@gmail.com>
2021-09-10 10:17:22 +0000

    Debug run py Format board

--- run.py
+++ run.py
@@ -4,9 +4,9 @@
 
 
 # Step 0 Build Board
-board = [[1, 2, 3],
-         [4, 5, 6],
-         [7, 8, 9]]
+board1 = [1, 2, 3]
+board2 = [4, 5, 6]
+board3 = [7, 8, 9]
 
 # Step 1 Check User Selection:
 print("User %d enter your move (1-9):")
