0ccb4f371b80fa36153f2e1b1a7b499e0f31511a
Coder731 <stephenobrien1@gmail.com>
2021-09-11 18:03:00 +0000

    Debug run py Make values a dictionary from list

--- run.py
+++ run.py
@@ -24,7 +24,7 @@ def print_board(values):
 
 # print_board()
 
-values = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+values = [a:1, b:2, c:3, d:4, e:5, f:6, g:7 h:8, i:9]
 # Function to print Tic Tac Toe
 # def print_tic_tac_toe(values):
 print("\n")
