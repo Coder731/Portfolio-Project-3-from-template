b3a9c1df5e86faae54fd3e1a18f8d58fd95df3fc
Coder731 <stephenobrien1@gmail.com>
2021-09-12 20:39:41 +0000

    run py Add list comprehension print statement

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
-# Credit Comment: 
+# Credit Comment:
 # https://youtu.be/BHh654_7Cmw?t=562
 # Step 0 Build Board
 board1 = [1, 2, 3]
@@ -12,6 +12,8 @@ board3 = [7, 8, 9]
 board = ["[ ]", "[ ]", "[ ]",
          "[ ]", "[ ]", "[ ]",
          "[ ]", "[ ]", "[ ]"]
+
+a={x: print(board[x],board[(x+1)],board[(x+2)]) for x in range(0,8,3)}
 
 # Step 1 Check User Selection:
 print("User enter your move (1-9):")
