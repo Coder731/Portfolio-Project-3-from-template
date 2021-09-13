c698bf4ec6dd099c3a54070f926d259b61846e31
Coder731 <stephenobrien1@gmail.com>
2021-09-13 14:05:06 +0000

    run3 py Add move variable and if statement

--- run3.py
+++ run3.py
@@ -4,3 +4,8 @@ new_board = {"a": "[ ]", "b": "[ ]", "c": "[ ]", "d": "[ ]",
 print(new_board["a"], new_board["b"], new_board["c"])
 print(new_board["d"], new_board["e"], new_board["f"])
 print(new_board["g"], new_board["h"], new_board["i"])
+
+move = input("Please enter a number from 1 to 9: ")
+
+if (move == 1):
+    new_board["a"] = "[ X ]"
