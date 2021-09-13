ae5d34cc5ba5242a2266dba96bc73a4b3a3d79e1
Coder731 <stephenobrien1@gmail.com>
2021-09-13 13:59:26 +0000

    run3 py Add file with code for development

---
+++ run3.py
@@ -0,0 +1,6 @@
+new_board = {"a": "[ ]", "b": "[ ]", "c": "[ ]", "d": "[ ]",
+             "e": "[ ]", "f": "[ ]", "g": "[ ]", "h": "[ ]", "i": "[ ]"}
+
+print(new_board["a"], new_board["b"], new_board["c"])
+print(new_board["d"], new_board["e"], new_board["f"])
+print(new_board["g"], new_board["h"], new_board["i"])
