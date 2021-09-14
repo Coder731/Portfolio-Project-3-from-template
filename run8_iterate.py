f4aeb15642fa9590857f11f6670a09456b7d14e5
Coder731 <stephenobrien1@gmail.com>
2021-09-14 20:14:02 +0000

    Debug run8 iterate py Add file Iterate board

---
+++ run8_iterate.py
@@ -0,0 +1,18 @@
+board = [
+        [' ', ' ', ' '],
+        [' ', ' ', ' '],
+        [' ', ' ', ' ']
+    ]
+
+r = 0
+c = 0
+
+    # index()
+    for r in board.index([r]):
+        for c in board.index([r][c]):
+            if board.index([r][c]) != " ":
+                print(f"board[{r}][{c}] is occupied.")
+            elif board.index([r][c]) == " ":
+                print(f"board[{r}][{c}] is first empty cell.")
+                print("game continues")
+                break
