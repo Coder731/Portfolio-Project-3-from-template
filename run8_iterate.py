5d66ad860291f3dc734fc486c045b63762d170c4
Coder731 <stephenobrien1@gmail.com>
2021-09-14 20:28:59 +0000

    minor run8 iterate py Unindent for index method
    Unindnet for loop index method

--- run8_iterate.py
+++ run8_iterate.py
@@ -7,12 +7,12 @@ board = [
 r = 0
 c = 0
 
-    # index()
-    for r in board.index([r]):
-        for c in board.index([r][c]):
-            if board.index([r][c]) != " ":
-                print(f"board[{r}][{c}] is occupied.")
-            elif board.index([r][c]) == " ":
-                print(f"board[{r}][{c}] is first empty cell.")
-                print("game continues")
-                break
+# index()
+for r in board.index([r]):
+    for c in board.index([r][c]):
+        if board.index([r][c]) != " ":
+            print(f"board[{r}][{c}] is occupied.")
+        elif board.index([r][c]) == " ":
+            print(f"board[{r}][{c}] is first empty cell.")
+            print("game continues")
+            break
