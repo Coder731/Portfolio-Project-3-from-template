66b0def63bde742cb9bee1039361b8dd1654ec52
Coder731 <stephenobrien1@gmail.com>
2021-09-15 22:00:49 +0000

    run9 is game over py Cut combo function

--- run9_is_game_over.py
+++ run9_is_game_over.py
@@ -58,61 +58,6 @@ def is_game_over():
     return False
 
 
-def combo():
-    # Returns
-    # TEST: No. of rows in board:  3
-    # TEST: No. of rows in board:  3
-
-    # row counter
-    row_throughput = np.array(board)
-    row_count = len(row_throughput)
-    print("TEST: No. of rows in board: ", row_count)
-    # col counter
-    col_throughput = np.array(board)
-    col_count = len(col_throughput)
-    print("TEST: No. of rows in board: ", col_count)
-
-    loop_count = 0
-
-    for row_i in row_throughput:
-        for col_j in col_throughput:
-            if board[r][c] == " ":
-                print("spaces available")
-                break
-            loop_count += 1
-            print('loop_count: ', loop_count)
-            print("row_i: ", row_i)
-            print("col_j: ", col_j)
-            print("len(row_i): ", len(row_i))
-            print("len(col_j): ", len(col_j))
-
-            # ilok()
-            # https://www.askpython.com/python-modules/pandas/update-the-value-of-a-row-dataframe
-            if row_i == row_throughput and col_j == col_throughput and \
-                    board.iloc[[r],[c]] != " ":
-                # board.iloc[[r],[c]][r][c] != " ":
-                print("no more spaces available, game over")
-                break
-
-            # above print returns:
-
-            # row_i:  [' ' ' ' ' ']
-            # col_j:  [' ' ' ' ' ']
-            # len(row_i):  3
-            # len(col_j):  3
-
-            # do something with row_i
-            # print((board[row_i][col_j]))
-
-            for row_var_2 in board:
-                # a
-                print("row_var_2: ", row_var_2)
-            print(board[0][0])
-
-
-combo()
-
-
 def print_board():
     for row in board:
         print("TEST2: row: ", row)
